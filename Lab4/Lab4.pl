c199c24218be5dbed8c04e365c693426a260170b
Nulianitskiy <nikitaul78@gmail.com>
2021-02-26 17:02:42 +0300

    L4_E3

--- Lab4/Lab4.pl
+++ Lab4/Lab4.pl
@@ -1,3 +1,4 @@
+% Задание 1
 writeList([]):-!.
 writeList([H|T]):-write(H),write(" "),writeList(T).
 
@@ -5,7 +6,7 @@ readList(N,L):-rList(N,0,[],L).
 rList(N,N,L,L):-!.
 rList(N,I,CurL,L):-I1 is I+1, read(X),append(CurL,[X],CurL1),rList(N,I1,CurL1,L).
 
-
+% Задание 2
 sumListDown(List,Sum):-sListD(List,0,Sum).
 sListD([],Sum,Sum):-!.
 sListD([H|L],CurSum,Sum):-CurSum1 is CurSum + H, sListD(L,CurSum1,Sum).
@@ -14,3 +15,7 @@ readSumWriteList(N):-
     readList(N,L),
     sumListDown(L,Sum),
     nl, write("Sum = "),write(Sum),nl,fail.
+
+% Задание 3
+sumListUp([],0):-!.
+sumListUp([H|List],Sum):-sumListUp(List,SumNew),Sum is SumNew + H.
