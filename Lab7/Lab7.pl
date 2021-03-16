d4afe33e4822d8b151c5fb0e23fc3ff30556d936
Nulianitskiy <nikitaul78@gmail.com>
2021-03-16 17:06:00 +0300

    L7_E2

--- Lab7/Lab7.pl
+++ Lab7/Lab7.pl
@@ -16,19 +16,20 @@ write_str([H|Tail]):-put(H),write_str(Tail).
 write_list_str([]):-!.
 write_list_str([H|T]):-write_str(H),nl,write_list_str(T).
 
-read_str(A,N,Flag):-get0(X),r_str(X,A,[],N,0,Flag).
-r_str(-1,A,A,N,N,1):-!.
-r_str(10,A,A,N,N,0):-!.
-r_str(X,A,B,N,K,Flag):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1,Flag).
-%read_str(A,N):-get0(X),r_str(X,A,[],N,0).
-%r_str(10,A,A,N,N):-!.
-% r_str(X,A,B,N,K):-K1 is
-% K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1).
+read_str_f(A,N,Flag):-get0(X),r_str_f(X,A,[],N,0,Flag).
+r_str_f(-1,A,A,N,N,1):-!.
+r_str_f(10,A,A,N,N,0):-!.
+r_str_f(X,A,B,N,K,Flag):-K1 is K+1,append(B,[X],B1),get0(X1),r_str_f(X1,A,B1,N,K1,Flag).
 
-read_list_str(List):-read_str(A,N,Flag),read_list_str([A],List,Flag).
+read_str(A,N):-get0(X),r_str(X,A,[],N,0).
+r_str(10,A,A,N,N):-!.
+ r_str(X,A,B,N,K):-K1 is
+ K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1).
+
+read_list_str(List):-read_str_f(A,_,Flag),read_list_str([A],List,Flag).
 read_list_str(List,List,1):-!.
 read_list_str(Cur_list,List,0):-
-	read_str(A,N,Flag),append(Cur_list,[A],C_l),read_list_str(C_l,List,Flag).
+	read_str_f(A,_,Flag),append(Cur_list,[A],C_l),read_list_str(C_l,List,Flag).
 
 in_list([El|_],El).
 in_list([_|T],El):-in_list(T,El).
@@ -37,31 +38,49 @@ in_list_exlude([El|T],El,T).
 in_list_exlude([H|T],El,[H|Tail]):-in_list_exlude(T,El,Tail).
 
 
-reverse(A, Z) :- reverse(A,Z,[]).
+reverse(A, Z):-reverse(A,Z,[]).
 reverse([],Z,Z).
-reverse([H|T],Z,Acc) :- reverse(T,Z,[H|Acc]).
+reverse([H|T],Z,Acc):-reverse(T,Z,[H|Acc]).
 
 remove_from_list(List, Elem, Result) :-
 	remove_from_list(List, Elem, [], Result).
 
-remove_from_list([IH|IT], Elem, TempLeft, Result) :-
+remove_from_list([IH|IT], Elem, TempLeft, Result):-
 	IH = Elem,
 	reverse(TempLeft, TempLeftReverse),
-
 	append(TempLeftReverse, IT, Result), !.
 
-remove_from_list([IH|IT], Elem, TempLeft, Result) :-
+remove_from_list([IH|IT], Elem, TempLeft, Result):-
 	not(IH = Elem),
 	remove_from_list(IT, Elem, [IH|TempLeft], Result).
 
 b_a_p_rep([],Perm1):-write_str(Perm1),nl,!,fail.
 b_a_p_rep(A,Perm):-unique_elems(A, Aun), in_list_exlude(Aun,El,Aun1),remove_from_list(A, El, A1),b_a_p_rep(A1,[El|Perm]).
 
-build_all_perm_rep(File) :-
+build_all_perm_rep(File):-
 	tell(File),
-	read_str(A,N),
+	read_str(A,_),
 	not(b_a_p_rep(A,[])),
 	told.
 
 % Exc 1
-prExc1:-read_str(A,X,0),write_str(A),write(" , "),write_str(A),write(" , "),write_str(A),write(" , "),write(X).
+prExc1:-read_str_f(A,X,0),write_str(A),write(" , "),write_str(A),write(" , "),write_str(A),write(" , "),write(X).
+
+% Exc 2
+prExc2:-read_str(A,_),count_words(A,K),write(K).
+
+count_words(A,K):-count_words(A,0,K).
+
+count_words([],K,K):-!.
+count_words(A,I,K):-skip_space(A,A1),get_word(A1,Word,A2),Word \=[],I1 is I+1,count_words(A2,I1,K),!.
+count_words(_,K,K).
+
+skip_space([32|T],A1):-skip_space(T,A1),!.
+skip_space(A1,A1).
+
+get_word([],[],[]):-!.
+get_word(A,Word,A2):-get_word(A,[],Word,A2).
+
+get_word([],Word,Word,[]).
+get_word([32|T],Word,Word,T):-!.
+get_word([H|T],W,Word,A2):-append(W,[H],W1),get_word(T,W1,Word,A2).
