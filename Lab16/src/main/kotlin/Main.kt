5e784511dcebcd1609e32c033cd8b92e229dc8ea
Nulianitskiy <nikitaul78@gmail.com>
2021-05-26 15:16:42 +0300

    L16_E2.4

--- Lab16/src/main/kotlin/Main.kt
+++ Lab16/src/main/kotlin/Main.kt
@@ -1,11 +1,15 @@
+import java.util.Objects.equals
+
 fun main(args: Array<String>){
-    println(findMaxInString(line))
+    println(palindromCheck(line))
 }
 val line = readLine()
-
+/// Exc 1
 fun findMaxInString(line:String?): String{
     val list = line!!.split(" ")
     var max = list[0].toInt()
     list.forEach{if(it.toInt()>max) max = it.toInt()}
     return max.toString()
 }
+/// Exc 2.4
+fun palindromCheck(line:String?):Boolean = equals(line,line!!.reversed())
