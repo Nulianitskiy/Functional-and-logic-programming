70f9c6b4ba592f092fc53a3e06b5888e04275e29
Nulianitskiy <nikitaul78@gmail.com>
2021-05-27 19:20:36 +0300

    L17_E3

--- Lab 17/src/main/kotlin/main.kt
+++ Lab 17/src/main/kotlin/main.kt
@@ -1,9 +1,23 @@
 fun main(args: Array<String>) {
-    printDoc(Docs(0,0,"Name","Surname","MiddleName","Male","00-00-0000","City"))
+    val doc1 = Docs(0,0,"Name","Surname","MiddleName","Male","00-00-0000","City")
+    val doc2 = Docs(1,1,"NoName","NoSurname","NoSurname","Male","01-01-0001","noCity")
+    val doc3 = Docs(2,1,"X","Y","Z","Male","00-00-0001","Geometry")
+    printDoc(doc1)
+    printDoc(doc2)
+    printDoc(doc3)
+    checkDocs(doc1, doc2)
+    checkDocs(doc2, doc3)
+    checkDocs(doc1, doc3)
 }
 class Docs(val series: Int, val number: Int, var firstName: String, var lastName:String, var middleName: String, var gender:String, val dateBirth: String, val cityOfBirth: String)
 
 fun printDoc(docs: Docs){
     println("Серия ${docs.series} Номер ${docs.number}\nФ.И.О. : ${docs.lastName} ${docs.firstName} ${docs.middleName}" +
-            "\nДата рождения : ${docs.dateBirth}\nГород рождения : ${docs.cityOfBirth}\nПол : ${docs.gender}")
+            "\nДата рождения : ${docs.dateBirth}\nГород рождения : ${docs.cityOfBirth}\nПол : ${docs.gender}\n\n")
+}
+
+fun checkDocs(doc1:Docs,doc2:Docs){
+    if(doc1.series == doc2.series || doc1.number == doc2.number)
+        println("Проблема! Цифры совпадают!\n")
+    else println("Все ок!\n")
 }
